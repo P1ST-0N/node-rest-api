9f02da2ca6a9c364a81d081d54edca8239fce5c7
P1ST-0N <luckylionia@gmail.com>
2024-04-25 23:06:50 +0300

    added contrl getAllContacts

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,8 +1,24 @@
-import contactsService from "../services/contactsServices.js";
+import {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+  try {
+    const contacts = await listContacts();
+    res.status(200).json(contacts);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = (req, res) => {
+  //   try {
+  //     const oneContact = getContactById(id);
+  //   } catch (error) {}
+};
 
 export const deleteContact = (req, res) => {};
 
